988377cc9b0efbebe079284bb1e9e189045d5d1f
mahdihma <mahdi.haji@yahoo.com>
2020-05-03 14:51:14 +0430

    shake sensor completed

--- app/src/main/java/com/example/hw2_mas/ShakeDetectorSensor.java
+++ app/src/main/java/com/example/hw2_mas/ShakeDetectorSensor.java
@@ -5,26 +5,55 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.PowerManager;
+import android.os.VibrationEffect;
 import android.os.Vibrator;
+import android.widget.Toast;
 
 public class ShakeDetectorSensor implements SensorEventListener {
     private Vibrator vibrator;
     private SensorManager shakeManager;
     private Context context;
     private Sensor shakeSensor;
-    private int TRESHOLD;
+    private float threshold = 25;
     private float currentAcceleration;
     private float lastAcceleration;
 
 
-    public ShakeDetectorSensor (Context context){
+    public ShakeDetectorSensor(Context context) {
         this.context = context;
         shakeManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        shakeSensor = shakeManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        shakeSensor = shakeManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         this.vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
     }
+
     @Override
     public void onSensorChanged(SensorEvent sensorEvent) {
+        lastAcceleration = currentAcceleration;
+        float x = sensorEvent.values[0];
+        float y = sensorEvent.values[1];
+        float z = sensorEvent.values[2];
+        currentAcceleration = (float) Math.sqrt(x * x + y * y + z * z);
+        if (currentAcceleration - lastAcceleration > threshold) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(400, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                vibrator.vibrate(400);
+            }
+            unlockPhone();
+            Toast.makeText(context, accelerationReport(), Toast.LENGTH_LONG).show();
+
+        }
+
+    }
+
+    private String accelerationReport() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("phone shake by ");
+        stringBuilder.append(currentAcceleration - lastAcceleration);
+        stringBuilder.append(" m/s^2 acceleration");
+        return stringBuilder.toString();
 
     }
 
@@ -33,11 +62,20 @@ public class ShakeDetectorSensor implements SensorEventListener {
 
     }
 
-    public void stopListening(){
+    public void stopListening() {
         shakeManager.unregisterListener(this);
     }
 
-    public void startListening(){
+    public void startListening() {
         shakeManager.registerListener(this, shakeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    private void unlockPhone() {
+        PowerManager powerManager = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
+        assert powerManager != null;
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.FULL_WAKE_LOCK |
+                PowerManager.ACQUIRE_CAUSES_WAKEUP | PowerManager.ON_AFTER_RELEASE,"appname::WakeLock");
+        wakeLock.acquire(5000 /*10 minutes*/);
+        wakeLock.release();
     }
 }
